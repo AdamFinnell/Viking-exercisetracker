a6cad86214a85f533a793396651f89b372fb0822
AdamFinnell <afinnell@perseverenow.org>
2025-06-18 16:23:14 -0500

    Sixth attempt

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
+console.log('MONGO_URI:', process.env.MONGO_URI);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
